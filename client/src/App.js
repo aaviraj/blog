4a57534db233913fe548e09ea42efc91377e600c
aaviraj <aviraj.bhattacharya2020@vitbhopal.ac.in>
2023-07-06 23:37:07 +0530

    3rd Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import Post from './Post';
-import Header from './Header';
 import {Route, Routes} from 'react-router-dom';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-         <main>
-         <Header />
-         <Post />
-         <Post />
-         <Post /> 
-       </main>
-
-      } />
-      <Route path={'/login'} element={
-        <div>login page</div>
-      }
-
-     />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={'/login'} element={<LoginPage />} />
+      </Route>
     </Routes>
 
    
