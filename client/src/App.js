76229d6d78a6bb19b6ad505b7c322ff37b92e229
aaviraj <aviraj.bhattacharya2020@vitbhopal.ac.in>
2023-07-07 08:45:26 +0530

    4th Commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom';
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={'/login'} element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
 
